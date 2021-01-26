b033b77f343d0a78310ecba7ad67428202f02f96
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2021-01-26 20:37:35 +0530

    3 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { EmployeeComponentComponent } from "./employee-component/employee-component.component";
 import { EmployeeDeatilsComponent } from "./employee-component/employee-deatils/employee-deatils.component";
-import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -14,8 +13,7 @@ import { EmployeeComponent } from './employee/employee.component';
     AppComponent,
     HelloComponent,
     EmployeeComponentComponent,
-    EmployeeDeatilsComponent,
-    EmployeeComponent
+    EmployeeDeatilsComponent
   ],
   bootstrap: [AppComponent]
 })
