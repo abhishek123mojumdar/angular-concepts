ef33318cdd14f949fb387296947a390cbedc2739
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2021-05-16 08:06:10 +0530

    2 files modified

--- src/app/employee-component/employee-component.component.ts
+++ src/app/employee-component/employee-component.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { employees } from "../data";
+import { Component, OnInit } from '@angular/core';
+import { employees } from '../data';
 
 @Component({
-  selector: "app-employee-component",
-  templateUrl: "./employee-component.component.html",
-  styleUrls: ["./employee-component.component.css"]
+  selector: 'app-employee-component',
+  templateUrl: './employee-component.component.html',
+  styleUrls: ['./employee-component.component.css']
 })
 export class EmployeeComponentComponent implements OnInit {
   constructor() {}
   public employee;
   public empDetail;
+  public twoWayBinding;
   ngOnInit() {
     this.employee = employees;
   }
@@ -17,24 +18,28 @@ export class EmployeeComponentComponent implements OnInit {
   getEmpStatus(per) {
     switch (per) {
       case 1:
-        return "Good";
+        return 'Good';
         break;
       case 2:
-        return "Above average";
+        return 'Above average';
         break;
       case 3:
-        return "Average";
+        return 'Average';
         break;
       case 4:
-        return "Needs work";
+        return 'Needs work';
         break;
       default:
-        return "No such rating exsists";
+        return 'No such rating exsists';
         break;
     }
   }
 
   checkEmployeeForHike(emp) {
     this.empDetail = emp;
+  }
+
+  getInputData(e) {
+    this.twoWayBinding = <HTMLInputElement>e.target.value;
   }
 }
