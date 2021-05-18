0296f81606c47d2c17ca1952e9a300a0bb9b2d9e
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2021-05-18 19:09:15 +0530

    1 file modified

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -4,9 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'temperature'
 })
 export class TemperaturePipe implements PipeTransform {
-
   transform(value: any, args?: any): any {
-    return null;
+    let temperature = value;
+    if (value && isNaN(value)) {
+      if (args === 'C') {
+        temperature = (value - 32) / 1.8;
+        return temperature.toFixed(2);
+      } else if (args === 'F') {
+        temperature = value * 1.8 + 32;
+        return temperature.toFixed(2);
+      }
+    } else {
+      return temperature;
+    }
   }
-
 }
