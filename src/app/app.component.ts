35ea080f9d0322e02ec3030d90d740fa0563f6d0
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2021-05-18 17:54:28 +0530

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  activation = {
+    employee: false
+  };
+
+  activate(type) {
+    if (type === 'employee') {
+      this.activation.employee = true;
+    }
+  }
 }
---
+++ src/app/temperature-conversion/temperature-conversion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-conversion',
+  templateUrl: './temperature-conversion.component.html',
+  styleUrls: ['./temperature-conversion.component.css']
+})
+export class TemperatureConversionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
