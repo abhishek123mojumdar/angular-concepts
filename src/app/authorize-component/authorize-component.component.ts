0cd6c46636df4516f341616ac2b5eacffb85a3c9
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2021-05-18 18:56:35 +0530

    2 files modified, 1 added

--- src/app/authorize-component/authorize-component.component.ts
+++ src/app/authorize-component/authorize-component.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
+  ElementRef,
   EventEmitter,
   OnInit,
   Output,
-  Renderer2
+  Renderer2,
+  ViewChild
 } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { authorize } from '../data';
@@ -17,7 +19,8 @@ export class AuthorizeComponentComponent implements OnInit {
   public authorizationData;
   public loginError = false;
   public loginErrMsg = '';
-  constructor() {}
+  constructor(private renderer: Renderer2) {}
+  @ViewChild('showErr', { static: true }) showErr: ElementRef;
   @Output() authorizedState = new EventEmitter<{ authorized: boolean }>();
   ngOnInit() {
     this.authorizationData = authorize;
@@ -30,13 +33,25 @@ export class AuthorizeComponentComponent implements OnInit {
 
   onSubmit() {
     let formData = this.loginForm.value;
+    let counter = 0;
     this.authorizationData.forEach(auth => {
       if (formData.userName == auth.name && formData.passWord == auth.pass)
         this.authorizedState.emit({ authorized: true });
       else {
-        this.loginError = true;
-        this.loginErrMsg = 'Some error in email id and password . Please check';
+        counter = counter + 1;
       }
     });
+
+    if (counter == this.authorizationData.length) {
+      const text = this.renderer.createText(
+        'Some error in email id and password . Please check'
+      );
+      this.renderer.appendChild(this.showErr.nativeElement, text);
+      this.renderer.setStyle(
+        this.showErr.nativeElement,
+        'background-color',
+        'red'
+      );
+    }
   }
 }
