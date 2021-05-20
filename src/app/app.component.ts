90347ee0cb2c3df67dcbda5747c65c1d99d5eabf
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2021-05-20 10:52:01 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
-
   activation = {
     employee: false,
     temperature: false
   };
   authorized = false;
+  public ngOnInit() {
+    if (
+      localStorage.getItem('auth') &&
+      localStorage.getItem('auth') === 'true'
+    ) {
+      this.authorized = true;
+    } else {
+      this.authorized = false;
+    }
+  }
 
   activate(type) {
     if (type === 'employee') {
@@ -26,5 +35,6 @@ export class AppComponent {
 
   checkAuthorized(e) {
     this.authorized = e.authorized;
+    localStorage.setItem('auth', this.authorized.toString());
   }
 }
